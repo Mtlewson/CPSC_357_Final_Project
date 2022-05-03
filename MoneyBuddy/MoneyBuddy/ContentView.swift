14daeaeaa6b4949599460b55c2508a38cb0d0224
Michael Lewson <78451053+Mtlewson@users.noreply.github.com>
2022-05-03 09:55:32 -0700

    added app

---
+++ MoneyBuddy/MoneyBuddy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoneyBuddy
+//
+//  Created by Michael Lewson on 5/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoneyBuddy/MoneyBuddy/MoneyBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyBuddyApp.swift
+//  MoneyBuddy
+//
+//  Created by Michael Lewson on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MoneyBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
